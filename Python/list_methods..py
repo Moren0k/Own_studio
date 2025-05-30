28091cf9bdde80b0fb270388e9d0bb9a40e8c239
Moren0k <jhoskevinagudelomoreno@gmail.com>
2025-05-30 11:35:31 -0500

    Commit

---
+++ Python/list_methods..py
@@ -0,0 +1,9 @@
+list = list(["Element0","Element1","Element2"]) #Crea una lista con List!
+
+count_list = len(list) #Devuelve la cantidad de elementos que hay en la lista!
+
+list.append("NewElement3") #Agrega el elemento a la lista al final!
+
+list.insert(2,"NewElementIn2Position") #Agrega el nuevo elemento en un indice/pocision especifica!
+
+list.extend(["NewElement4","NewElement5"]) #Agrega varios elementos nuevos a la lista en formato de lista (Agrega una lista a otra Lista)
